a8be830a15f9b636d14b280488e684af4dcbff78
Varun Reddy <varunreddymullangi@gmail.com>
2023-08-30 18:03:23 -0700

    2.4.1

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -8,20 +8,32 @@
 
 #include <stdbool.h>
 
-struct elfhdr* kernel_elfhdr;
-struct proghdr* kernel_phdr;
+struct elfhdr *kernel_elfhdr;
+struct proghdr *kernel_phdr;
 
-uint64 find_kernel_load_addr(enum kernel ktype) {
+uint64 find_kernel_load_addr(enum kernel ktype)
+{
     /* CSE 536: Get kernel load address from headers */
-    return 0;
+    kernel_elfhdr = (struct elfhdr *)RAMDISK;
+
+    uint64 phoff = kernel_elfhdr->phoff;
+    uint64 phsize = kernel_elfhdr->phentsize;
+
+    uint64 text_phdr_addr = RAMDISK + phoff + phsize;
+
+    kernel_phdr = (struct proghdr *)text_phdr_addr;
+
+    return kernel_phdr->vaddr;
 }
 
-uint64 find_kernel_size(enum kernel ktype) {
+uint64 find_kernel_size(enum kernel ktype)
+{
     /* CSE 536: Get kernel binary size from headers */
     return 0;
 }
 
-uint64 find_kernel_entry_addr(enum kernel ktype) {
+uint64 find_kernel_entry_addr(enum kernel ktype)
+{
     /* CSE 536: Get kernel entry point from headers */
     return 0;
 }
